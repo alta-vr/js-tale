2641de82356a1c358ace05fd7397d8ba4e22ab21
Narmdo <narmdo@gmail.com>
2021-02-21 18:23:30 +1100

    Recompiled

--- dist/Helpers/AttCore.d.ts
+++
@@ -1,7 +0,0 @@
-import { ApiConnection, SubscriptionManager, GroupManager, Config } from "..";
-export default class AttCore {
-    api: ApiConnection;
-    subscriptions: SubscriptionManager;
-    groupManager: GroupManager;
-    init(config: Config): Promise<void>;
-}
--- dist/Helpers/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { default as AttCore } from './AttCore';
-export { default as AttSimple, AttSimpleConfig } from './AttSimple';
