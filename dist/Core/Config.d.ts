20a78b3c0027f82cd6edfe299c1886e982b8bc50
Marc Dingena <marc.dingena@gmail.com>
2021-05-20 13:18:57 +0800

    Ignore built files in source version history

--- dist/Core/Config.d.ts
+++
@@ -1,7 +0,0 @@
-export default interface Config {
-    client_id: string;
-    client_secret: string;
-    scope: string;
-    tokenHost?: string;
-    endpoint?: string;
-}
--- dist/Groups/GroupMemberInvite.d.ts
+++
@@ -1,6 +0,0 @@
-import GroupMember from './GroupMember';
-import Group from "./Group";
-export default class GroupMemberInvite extends GroupMember {
-    constructor(group: Group, info: any);
-    revoke(): Promise<PromiseLike<never>>;
-}
